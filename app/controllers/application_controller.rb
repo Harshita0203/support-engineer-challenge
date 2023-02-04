ba61265aff97d1b55319621fd80ba7ab142a829c
Harshita Gupta <47516177+Harshita0203@users.noreply.github.com>
2023-02-04 16:30:55 +0530

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+      def create
+        # send the email using the ApplicationMailer class
+        ApplicationMailer.email.deliver_now
+    
+        # redirect the user to the success page
+        redirect_to mail_path
+     end
 end
